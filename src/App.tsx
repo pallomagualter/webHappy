4d10fe8cc3ec7d058fe954c12a3c089c23d8c20a
Palloma Gualter <pallomagualter@gmail.com>
2020-10-19 16:07:26 -0300

    style global

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import './styles/global.css';
+
+function Title() {
+  return <h1>Hello... it's me!</h1>
+}
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <Title />
     </div>
   );
 }
