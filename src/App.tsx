ea8e2816b3bf9d8aeececb3a193f3875603e5911
Palloma Gualter <pallomagualter@gmail.com>
2020-10-20 11:22:03 -0300

    landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
 
 import './styles/global.css';
+import './styles/pages/landing.css';
 
-function Title() {
-  return <h1>Hello... it's me!</h1>
-}
+import logoImg from './assets/images/logo.svg';
 
 function App() {
   return (
-    <div className="App">
-      <Title />
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy"/>
+      </div>
+
+      <main>
+        <h1>Leve felicidade para o mundo</h1>
+        <p>Visite casas de acolhimento e mude o dia de muitas crianças.</p>
+      </main>
+
+      <div className="location">
+        <strong>Teresina</strong>
+        <span>Piauí</span>
+      </div>
+
+      <a href="" className="enter-app"
+        >
+
+      </a>
     </div>
   );
 }
