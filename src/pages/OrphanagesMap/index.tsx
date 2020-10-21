7f8036e27061fa64d4bd58c4834a710f92b2ec38
Palloma Gualter <pallomagualter@gmail.com>
2020-10-21 10:16:15 -0300

    map

--- src/pages/OrphanagesMap/index.tsx
+++ src/pages/OrphanagesMap/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiPlus } from 'react-icons/fi';
-import { Map } from 'react-leaflet';
+import { Map, TileLayer } from 'react-leaflet';
+
+import 'leaflet/dist/leaflet.css';
 
 import mapMarkerImg from '../../images/map-marker.svg';
 
@@ -24,7 +26,13 @@ function OrphanagesMap() {
                 </footer>
             </aside>
 
-            <div className="map"></div>
+            <Map
+                center={[-5.0819027,-42.7858102]} //latitude, longitude
+                zoom={17}
+                style={{ width: '100%', height: '100%' }}
+            >
+                <TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+            </Map>
 
             <Link to="/" className="create-orphanage" >
                 <FiPlus size={32} color="#fff" />
