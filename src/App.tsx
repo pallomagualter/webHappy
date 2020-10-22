6b6ce128e7c53a878b3c0353a234fccb0b814ed2
Palloma Gualter <pallomagualter@gmail.com>
2020-10-22 10:05:25 -0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Routes from './routes';
 
-//import 'leaflet/dist/leaflet.css';
+import 'leaflet/dist/leaflet.css';
 import './styles/global.css';
 
 export default function App() {
